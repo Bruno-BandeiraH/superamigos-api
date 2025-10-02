1c212796504c44105de5dcb501a85e059ae8d6c2
Bruno <brunohenrique0665@gmail.com>
2025-10-01 22:02:59 -0300

    first commit

---
+++ src/test/java/com/superamigos/SuperamigosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superamigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperamigosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
