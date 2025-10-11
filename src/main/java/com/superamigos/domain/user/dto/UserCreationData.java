b858cca61bdaae13bef2e2dbf22c99d673af4701
Bruno <brunohenrique0665@gmail.com>
2025-10-11 03:50:03 -0300

    refactor: adding min and max caracters in password

--- src/main/java/com/superamigos/domain/user/dto/UserCreationData.java
+++ src/main/java/com/superamigos/domain/user/dto/UserCreationData.java
@@ -1,9 +1,10 @@
 package com.superamigos.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserCreationData(
     @NotBlank String name,
     @NotBlank String username,
-    @NotBlank String password) {
+    @NotBlank @Size(min = 6, max = 18) String password) {
 }
